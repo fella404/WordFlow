0b69dd1de31c7e2f28202695845d658e397ac195
fella404 <tomhagen849@gmail.com>
2025-10-07 07:40:46 +0700

    feat: Add cors in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,11 @@ const env = dotenv.config({ quiet: true }).parsed;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http:localhost:5173",
+  })
+);
 app.use(express.json());
 
 app.use("/api", router);
