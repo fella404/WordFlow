ff2a6f7bfa99804e9a2cc4c88f3baf8d43950ac8
fella404 <tomhagen849@gmail.com>
2025-12-02 15:37:25 +0700

    commit for deployment project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import adminRoutes from "./routes/adminRoutes.js";
 import publicRoutes from "./routes/publicRoutes.js";
@@ -9,6 +10,8 @@ import connection from "./config/db.js";
 const env = dotenv.config({ quiet: true }).parsed;
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -19,6 +22,14 @@ app.use(express.json());
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/public", publicRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 connection().then(() => {
   app.listen(env.APP_PORT || 3000, () => {
