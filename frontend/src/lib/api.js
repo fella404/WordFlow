5ecebce72810475711bf163b439d8936743dbd46
fella404 <tomhagen849@gmail.com>
2026-03-09 15:01:28 +0700

    chore(api): change baseURL axios

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  baseURL:
+    import.meta.env.VITE_ENV === "production"
+      ? "/api"
+      : "http://localhost:3000/api",
   withCredentials: true,
 });
 
